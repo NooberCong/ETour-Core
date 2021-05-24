c210e127a5a40643a0c9c95eda06286cb3d6219f
NooberCong <inuyashathanhcongnguyen@gmail.com>
2021-05-24 12:01:17 +0700

    Removed unnecessary files

--- Interfaces/IEmployee.cs
+++ Interfaces/IEmployee.cs
@@ -4,7 +4,7 @@ namespace Core.Interfaces
 {
     public interface IEmployee: IEntityWithKey<string>
     {
-        public string Name { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
         public DateTime DOB { get; set; }
         public DateTime StartWork { get; set; }
