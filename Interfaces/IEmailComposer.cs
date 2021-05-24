485b55f36203655090df02fe0b5d202dd311109f
NooberCong <inuyashathanhcongnguyen@gmail.com>
2021-05-24 18:52:46 +0700

    Interface for composing email added

---
+++ Interfaces/IEmailComposer.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IEmailComposer
+    {
+        public string ComposeConfirmEmail(string name, string confirmUrl);
+        public string ComposeResetPassword(string name, string confirmUrl);
+        public string ComposeBookingConfirmation(Booking booking);
+        public string ComposeBookingCancelation(Booking booking);
+
+    }
+}
