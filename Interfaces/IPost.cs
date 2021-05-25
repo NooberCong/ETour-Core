f71d69b0591733a097ec462e235ab5ec5d63d40e
NooberCong <inuyashathanhcongnguyen@gmail.com>
2021-05-25 12:21:25 +0700

    Add Title to IPost

--- Interfaces/IPost.cs
+++ Interfaces/IPost.cs
@@ -4,6 +4,7 @@ namespace Core.Entities
 {
     public interface IPost<TAuthor> : IEntityWithKey<int>, IDeleteEntity, ITrackedEntity, IAuthoredEntity<TAuthor> where TAuthor : IEmployee
     {
+        public string Title { get; set; }
         public string Content { get; set; }
         public string CoverImgUrl { get; set; }
     }
