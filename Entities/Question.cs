3392dda90a118f1fce83c73bc8b72983b26ce143
NooberCong <inuyashathanhcongnguyen@gmail.com>
2021-06-07 18:52:53 +0700

    Update Question entity, added Title, Category, Priority and Status

--- Entities/Question.cs
+++ Entities/Question.cs
@@ -3,9 +3,36 @@ using System.Collections.Generic;
 
 namespace Core.Entities
 {
-    public class Question : AuthoredTrackedDeleteEntityWithKey<Customer, int, string>
+    public class Question : AuthoredTrackedEntityWithKey<Customer, int, string>
     {
+        public string Title { get; set; }
         public string Content { get; set; }
+        public QuestionCategory Category { get; set; }
+        public QuestionPriority Priority { get; set; }
+        public QuestionStatus Status { get; set; }
+
         public ICollection<Answer> Answers { get; set; }
+
+        public enum QuestionCategory
+        {
+            Account,
+            Travel,
+            Order,
+            Other
+        }
+
+        public enum QuestionPriority
+        {
+            High,
+            Medium,
+            Low
+        }
+
+        public enum QuestionStatus
+        {
+            Pending,
+            Open,
+            Closed
+        }
     }
 }
