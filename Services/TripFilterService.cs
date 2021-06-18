cd9bc7e95aeea97f945e442bd6e2771f0e6bf793
NooberCong <inuyashathanhcongnguyen@gmail.com>
2021-06-18 08:30:27 +0700

    Tour Type Trip Filter implemented, upgrade keyword filter to use fuzzy matching algorithm

--- Services/TripFilterService.cs
+++ Services/TripFilterService.cs
@@ -1,19 +1,24 @@
 ﻿using Core.Entities;
 using Core.Value_Objects;
+using FuzzySharp;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 namespace Core.Services
 {
     public class TripFilterService
     {
-        public Func<Trip, bool> BuildFilterPredicate(TripFilterParams filterParams)
+        public IEnumerable<Trip> ApplyFilter(IEnumerable<Trip> trips, TripFilterParams filterParams)
         {
-            return trip => (filterParams.TourID == null || trip.TourID == filterParams.TourID)
-            && (string.IsNullOrWhiteSpace(filterParams.Keyword) || trip.Tour.Title.Contains(filterParams.Keyword, StringComparison.OrdinalIgnoreCase) || trip.Tour.Description.Contains(filterParams.Keyword, StringComparison.OrdinalIgnoreCase))
+            return trips.Where(trip => (filterParams.TourID == null || trip.TourID == filterParams.TourID)
+            && (string.IsNullOrWhiteSpace(filterParams.Keyword) || Fuzz.PartialTokenSetRatio(trip.Tour.Title, filterParams.Keyword) >= 50 || Fuzz.PartialTokenSetRatio(trip.Tour.Description, filterParams.Keyword) >= 50)
+            && (filterParams.TourType == null || trip.Tour.Type == filterParams.TourType)
             && (filterParams.Starts == null || trip.StartTime.Date == filterParams.Starts.Value.Date)
             && (filterParams.Ends == null || trip.EndTime.Date == filterParams.Ends.Value.Date)
             && (filterParams.MinPrice == null || trip.GetSalePrice() >= filterParams.MinPrice)
-            && (filterParams.MaxPrice == null || trip.GetSalePrice() <= filterParams.MaxPrice);
+            && (filterParams.MaxPrice == null || trip.GetSalePrice() <= filterParams.MaxPrice))
+                .OrderBy(trip => string.IsNullOrEmpty(filterParams.Keyword) ? 0: -Fuzz.PartialTokenSetRatio(trip.Tour.Title, filterParams.Keyword) * 1.5 - Fuzz.PartialTokenSetRatio(trip.Tour.Description, filterParams.Keyword));
         }
     }
 }
--- Value Objects/TripFilterParams.cs
+++ Value Objects/TripFilterParams.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace Core.Value_Objects
     {
         public string Keyword { get; set; }
         public int? TourID { get; set; }
+        public Tour.TourType? TourType { get; set; }
         [DataType(DataType.Date)]
         public DateTime? Starts { get; set; }
         [DataType(DataType.Date)]
