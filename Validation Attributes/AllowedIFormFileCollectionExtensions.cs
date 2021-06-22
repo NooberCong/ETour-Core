4b8a20dfbdb475794eab28ffeb167f15037d786e
NooberCong <inuyashathanhcongnguyen@gmail.com>
2021-06-22 15:46:51 +0700

    File extensions validations added

---
+++ Validation Attributes/AllowedIFormFileCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+
+namespace Core.Validation_Attributes
+{
+    public class AllowedIFormFileCollectionExtensions : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+
+        public AllowedIFormFileCollectionExtensions(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var files = value as IFormFileCollection;
+            foreach (var file in files)
+            {
+                var extension = Path.GetExtension(file.FileName);
+                if (file != null)
+                {
+                    if (!_extensions.Contains(extension.ToLower()))
+                    {
+                        return new ValidationResult(GetErrorMessage());
+                    }
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage()
+        {
+            return $"File's type is not valid.";
+        }
+    }
+}
+
+
---
+++ Validation Attributes/AllowedIFormFileExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Validation_Attributes
+{
+    public class AllowedIFormFileExtensions : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+
+        public AllowedIFormFileExtensions(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            var extension = Path.GetExtension(file.FileName);
+            if (file != null)
+            {
+                if (!_extensions.Contains(extension.ToLower()))
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage()
+        {
+            return $"File's type is not valid.";
+        }
+    }
+}
